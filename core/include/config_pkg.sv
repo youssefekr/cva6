b2b43d416d3d578412692ffc6a8f91c3869f919e
Youssef El Kaisi <youssefek@outlook.com>
2026-01-19 14:49:26 +0100

    cnofig pkg

--- core/include/config_pkg.sv
+++ core/include/config_pkg.sv
@@ -84,8 +84,6 @@ package config_pkg;
     bit                          RVZCMT;
     // Zicond RISC-V extension
     bit                          RVZiCond;
-    // Zicbom RISC-V extension (cache management / CBO)
-    bit                          RVZiCbom;
     // Zicntr RISC-V extension
     bit                          RVZicntr;
     // Zihpm RISC-V extension
@@ -120,12 +118,6 @@ package config_pkg;
     logic [63:0]                 HaltAddress;
     // Address to jump when exception
     logic [63:0]                 ExceptionAddress;
-    // Trigger Module Sdtrig Extension
-    bit                          SDTRIG;
-    bit                          Mcontrol6;
-    bit                          Icount;
-    bit                          Etrigger;
-    bit                          Itrigger;
     // Tval Support Enable
     bit                          TvalEn;
     // MTVEC CSR supports only direct mode
@@ -192,77 +184,60 @@ package config_pkg;
     int unsigned                 DcacheSetAssoc;
     // Data cache line width
     int unsigned                 DcacheLineWidth;
-    // three configurations for cache coherency after flush:
-    // DcacheFlushOnFence causes dcache flush for every fence instruction
-    // DcacheFlushOnFenceI causes dcache flush for every fence.I instruction
-    // DcacheInvalidateOnFlush causes dcache to also be invalidated when flushed
-    // tradeoff between coherence and efficiency, depending on remaining configuration:
-
-    // DcacheFlushOnFenceI is required for write-back caches - otherwise, 
-    // no way to reliably write instruction memory with store instructions, 
-    // as data and instruction cache are currently not coherent
-    // DcacheFlushOnFence is required for write-back caches to ensure coherency
-    // with other harts or DMA devices --> a fence forces all stores to commit to memory
-    // DcacheInvalidateOnFlush causes all dcache entries to become invalid, forcing the CPU
-    // to fetch data from memory after each fence --> make writes from other harts or DMAs
-    // visible to the CPU
-    // thus, DcacheFlushOnFence and DcacheInvalidateOnFlush can ensure DMA coherency at high performance cost
-    // using RVZiCbom can achieve the same effect at significantly lower performance cost
-    // hence, on uniprocessor or not cache-coherent multiprocessor SoCs, one might want to disable both and use
-    // explicit CBO operations for better overall performance
-    bit          DcacheFlushOnFence;
-    bit          DcacheFlushOnFenceI;
-    bit          DcacheInvalidateOnFlush;
+    // Data cache flush on fence
+    bit                          DcacheFlushOnFence;
+    // Data cache invalidate on flush
+    bit                          DcacheInvalidateOnFlush;
     // User field on data bus enable
-    int unsigned DataUserEn;
+    int unsigned                 DataUserEn;
     // Write-through data cache write buffer depth
-    int unsigned WtDcacheWbufDepth;
+    int unsigned                 WtDcacheWbufDepth;
     // User field on fetch bus enable
-    int unsigned FetchUserEn;
+    int unsigned                 FetchUserEn;
     // Width of fetch user field
-    int unsigned FetchUserWidth;
+    int unsigned                 FetchUserWidth;
     // Is FPGA optimization of CV32A6 for Xilinx and Altera
-    bit          FpgaEn;
+    bit                          FpgaEn;
     // Is FPGA optimization for Altera FPGA
-    bit          FpgaAlteraEn;
+    bit                          FpgaAlteraEn;
     // Is Techno Cut instantiated
-    bit          TechnoCut;
+    bit                          TechnoCut;
+
+    bit                          MultihartEn;
     // Enable superscalar* with 2 issue ports and 2 commit ports.
-    bit          SuperscalarEn;
+    bit                          SuperscalarEn;
     // Enable ALU-ALU bypass (superscalar mode only)
-    bit          ALUBypass;
+    bit                          ALUBypass;
     // Number of commit ports. Forced to 2 if SuperscalarEn.
-    int unsigned NrCommitPorts;
+    int unsigned                 NrCommitPorts;
     // Load cycle latency number
-    int unsigned NrLoadPipeRegs;
+    int unsigned                 NrLoadPipeRegs;
     // Store cycle latency number
-    int unsigned NrStorePipeRegs;
+    int unsigned                 NrStorePipeRegs;
     // Scoreboard length
-    int unsigned NrScoreboardEntries;
+    int unsigned                 NrScoreboardEntries;
     // Load buffer entry buffer
-    int unsigned NrLoadBufEntries;
+    int unsigned                 NrLoadBufEntries;
     // Maximum number of outstanding stores
-    int unsigned MaxOutstandingStores;
+    int unsigned                 MaxOutstandingStores;
     // Return address stack depth
-    int unsigned RASDepth;
+    int unsigned                 RASDepth;
     // Branch target buffer entries
-    int unsigned BTBEntries;
+    int unsigned                 BTBEntries;
     // Branch predictor type
-    bp_type_t    BPType;
+    bp_type_t                    BPType;
     // Branch history entries
-    int unsigned BHTEntries;
+    int unsigned                 BHTEntries;
     // Branch history bits
-    int unsigned BHTHist;
+    int unsigned                 BHTHist;
     // MMU instruction TLB entries
-    int unsigned InstrTlbEntries;
+    int unsigned                 InstrTlbEntries;
     // MMU data TLB entries
-    int unsigned DataTlbEntries;
+    int unsigned                 DataTlbEntries;
     // MMU option to use shared TLB
-    bit unsigned UseSharedTlb;
+    bit unsigned                 UseSharedTlb;
     // MMU depth of shared TLB
-    int unsigned SharedTlbDepth;
-    // Option to enable Svnapot extension
-    bit          SvnapotEn;
+    int unsigned                 SharedTlbDepth;
   } cva6_user_cfg_t;
 
   typedef struct packed {
@@ -280,6 +255,9 @@ package config_pkg;
     bit FpgaAlteraEn;
     bit TechnoCut;
 
+    bit          MultihartEn;
+    int unsigned NrHarts;
+    int unsigned LOG2_HARTS;
     bit          SuperscalarEn;
     int unsigned NrCommitPorts;
     int unsigned NrIssuePorts;
@@ -315,7 +293,6 @@ package config_pkg;
     bit          CvxifEn;
     copro_type_t CoproType;
     bit          RVZiCond;
-    bit          RVZiCbom;
     bit          RVZicntr;
     bit          RVZihpm;
 
@@ -348,7 +325,6 @@ package config_pkg;
     int unsigned InstrTlbEntries;
     int unsigned DataTlbEntries;
     bit unsigned UseSharedTlb;
-    bit SvnapotEn;
     int unsigned SharedTlbDepth;
     int unsigned VpnLen;
     int unsigned PtLevels;
@@ -373,11 +349,6 @@ package config_pkg;
     logic [NrMaxRules-1:0][63:0] CachedRegionLength;
     int unsigned                 MaxOutstandingStores;
     bit                          DebugEn;
-    bit                          SDTRIG;
-    bit                          Mcontrol6;
-    bit                          Icount;
-    bit                          Etrigger;
-    bit                          Itrigger;
     bit                          NonIdemPotenceEn;       // Currently only used by V extension (Ara)
     bit                          AxiBurstWriteEn;
 
@@ -402,7 +373,6 @@ package config_pkg;
     int unsigned DCACHE_MAX_TX;
 
     bit DcacheFlushOnFence;
-    bit DcacheFlushOnFenceI;
     bit DcacheInvalidateOnFlush;
 
     int unsigned DATA_USER_EN;
@@ -452,6 +422,7 @@ package config_pkg;
     assert (Cfg.NrExecuteRegionRules <= NrMaxRules);
     assert (Cfg.NrCachedRegionRules <= NrMaxRules);
     assert (Cfg.NrPMPEntries <= 64);
+    assert (!(Cfg.SuperscalarEn && Cfg.RVF));
     assert (Cfg.FETCH_WIDTH == 32 || Cfg.FETCH_WIDTH == 64)
     else $fatal(1, "[frontend] fetch width != not supported");
     // Support for disabling MIP.MSIP and MIE.MSIE in Hypervisor and Supervisor mode is not supported
